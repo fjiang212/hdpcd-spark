4724a7ff6bd5847a10d4a0281fcc9b3876b196a4
Feng Jiang <feng.jiang@bmo.com>
2017-02-13 17:03:34 -0500

    Create RDD

---
+++ src/main/scala/core/CreateRDD.scala
@@ -0,0 +1,14 @@
+package core
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object CreateRDD {
+    def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("CreateRDD").setMaster("local")
+    val sc = new SparkContext(conf)
+    val data = Array(1, 2, 3, 4, 5)
+    val dataRDD = sc.parallelize(data)
+    dataRDD.collect().foreach(println)
+  }
+}
